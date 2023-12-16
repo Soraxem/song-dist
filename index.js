58e7eec1c6ed8f50e7e7f03244bf98521d6d7a27
Elija-K <elija-kaeser@proton.me>
2023-12-16 13:31:57 +0100

    changed the script for the PUSH notify

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const check = () => {
-    if (!('serviceWorker' in navigator)) {
-      throw new Error('Kein Service Worker unterstützt!')
-    }
-    if (!('PushManager' in window)) {
-      throw new Error('Keine PUSH API unterstützt!')
-    }
-  }
-  const main = () => {
-    check()
-  main()
-  }
-
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js').then(function() {
+    return navigator.serviceWorker.ready;
+  })
+  .then(function(registration) {
+    console.log(registration); // service worker is ready and working...
+  });
+  
+  navigator.serviceWorker.addEventListener('message', function(event) {
+    console.log(event.data.message); // Hello World !
+  });
+}
 
 function asknotify() {
     Notification.requestPermission();
--- sw.js
+++ sw.js
@@ -2,6 +2,42 @@
 layout: none
 ---
 
+function messageToClient(client, data) {
+  return new Promise(function(resolve, reject) {
+    const channel = new MessageChannel();
+
+    channel.port1.onmessage = function(event){
+      if (event.data.error) {
+        reject(event.data.error);
+      } else {
+        resolve(event.data);
+      }
+    };
+
+    client.postMessage(JSON.stringify(data), [channel.port2]);
+  });
+}
+
+self.addEventListener('push', function (event) {
+  if (event && event.data) {
+    self.pushData = event.data.json();
+    if (self.pushData) {
+      event.waitUntil(self.registration.showNotification(self.pushData.title, {
+        body: self.pushData.body,
+        icon: self.pushData.data ? self.pushData.data.icon : null
+      }).then(function() {
+        clients.matchAll({type: 'window'}).then(function (clientList) {
+          if (clientList.length > 0) {
+            messageToClient(clientList[0], {
+              message: texttext // suppose it is: "Hello World !"
+            });
+          }
+        });
+      }));
+    }
+  }
+});
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 workbox.setConfig({
